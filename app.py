fe7c09d281765ac4256b603450037a04b55c86b2
Tomasz Zapart <tomasz.t.zapart@gmail.com>
2025-10-11 21:48:37 +0200

    ml model

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-
+import inference
+from inference import load_model
+from api.models.iris import PredictResponse, PredictRequest
 
 app = FastAPI()
+
+try:
+    model = load_model("trained_model.joblib")
+except FileNotFoundError:
+    print("Please run 'python training.py' first to create the model file.")
+    raise FileNotFoundError("filed does not exist")
 
 
 @app.get("/")
@@ -12,3 +20,16 @@ def welcome_root():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+
+@app.post("/predict")
+def predict(request: PredictRequest) -> PredictResponse:
+    input_data = [
+        request.sepal_length,
+        request.sepal_width,
+        request.petal_length,
+        request.petal_width,
+    ]
+    prediction = inference.predict(model, input_data)
+
+    return PredictResponse(prediction=prediction)
